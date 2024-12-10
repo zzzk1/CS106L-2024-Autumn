26897586c88f7a66d9c8a4c405b25b1bdfd6af6b
zzzk1 <madzhou1@gmail.com>
2024-12-10 22:48:58 +0800

    Assignment 4

--- assign4/main.cpp
+++ assign4/main.cpp
@@ -20,27 +20,50 @@ struct Forecast {
 };
 
 Forecast compute_forecast(const std::vector<double>& dailyWeather) {
-  // STUDENT TODO 1: return a forecast for the daily weather that is passed in.
+  Forecast forecast {};
+
+  forecast.min_temp = *std::min_element(dailyWeather.begin(), dailyWeather.end());
+  forecast.max_temp = *std::max_element(dailyWeather.begin(), dailyWeather.end());
+  forecast.avg_temp = std::accumulate(dailyWeather.begin(), dailyWeather.end(), 0.0) / (double)dailyWeather.size();
+
+  return forecast;
 }
 
 std::vector<Forecast> get_forecasts(const std::vector<std::vector<double>>& weatherData) {
-  /*
-   * STUDENT TODO 2: returns a vector of Forecast structs for the weatherData which contains
-   * std::vector<double> which contain values for the weather on that day.
-   */
+  std::vector<Forecast> result;
+  std::transform(weatherData.begin(), weatherData.end(), std::back_inserter(result), compute_forecast);
+  return result;
 }
 
 std::vector<Forecast> get_filtered_data(const std::vector<Forecast>& forecastData) {
-  // STUDENT TODO 3: return a vector with Forecasts filtered for days with specific weather.
+  auto filter_data = [](Forecast& forecast) {
+    return (forecast.max_temp - forecast.min_temp <= kMaxTempRequirement) || (forecast.avg_temp < uAvgTempRequirement);
+  };
+
+  std::vector<Forecast> result = forecastData;
+  result.erase(std::remove_if(result.begin(), result.end(), filter_data), result.end());
+
+  return result;
 }
 
 
 std::vector<Forecast> get_shuffled_data(const std::vector<Forecast>& forecastData) {
-  // STUDENT TODO 4: Make use of a standard library algorithm to shuffle the data!
+  std::random_device rd;
+  std::mt19937 g(rd());
+
+  std::vector<Forecast> result = forecastData;
+  std::shuffle(result.begin(), result.end(), g);
+
+  return result;
 }
 
 std::vector<Forecast> run_weather_pipeline(const std::vector<std::vector<double>>& weatherData) {
-  // STUDENT TODO 5: Put your functions together to run the weather pipeline!
+  std::vector<Forecast> result = get_forecasts(weatherData);
+  std::cout << "Before filtering: " << result.size() << " forecasts.\n";
+  result = get_filtered_data(result);
+  std::cout << "After filtering: " << result.size() << " forecasts.\n";
+  result = get_shuffled_data(result);
+  return result;
 }
 
 /* #### Please don't change this line! #### */
